51c9a2e6328153ed20eddff44bc8504d58975445
Pavel.Balozhka <Pavel.Balozhka@itechart-group.com>
2022-07-14 11:07:26 +0300

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'node:path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const DOC_API = await readFile(
+    join(
+      'D:\\projects\\node-js-course\\nodejs2022Q2-service',
+      'doc',
+      'api.yaml',
+    ),
+    'utf-8',
+  );
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
